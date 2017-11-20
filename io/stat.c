johnpgarry/xfsprogs-dev

commit b4056fed6eda8180153fb274733aec8ff5dd14c9

xfs_io: Print filesystem statfs flags in 'statfs' command
Sometimes printing the flags from the statfs structure is useful, so,
make statfs command print them.

Signed-off-by: Carlos Maiolino <cmaiolino@redhat.com>
Reviewed-by: Bill O'Donnell <billodo@redhat.com>
Signed-off-by: Eric Sandeen <sandeen@sandeen.net>

io/stat.c
@@ -199,6 +199,7 @@ statfs_f(
 		printf(_("statfs.f_bavail = %lld\n"), (long long) st.f_bavail);
 		printf(_("statfs.f_files = %lld\n"), (long long) st.f_files);
 		printf(_("statfs.f_ffree = %lld\n"), (long long) st.f_ffree);
+		printf(_("statfs.f_fflags = 0x%llx\n"), (long long) st.f_flags);
 	}
 	if (file->flags & IO_FOREIGN)
 		return 0;

commit 8bb344f4bf2ef5d90bc02221d8411db603c11416

xfs_io: stat: fix typo in statfs->f_flags
The field name is f_flags.

Fixes: b4056fed "xfs_io: Print filesystem statfs flags in 'statfs' command"
Signed-off-by: Jeff Mahoney <jeffm@suse.com>
Reviewed-by: Eric Sandeen <sandeen@redhat.com>
Signed-off-by: Eric Sandeen <sandeen@sandeen.net>

io/stat.c
@@ -199,7 +199,7 @@ statfs_f(
 		printf(_("statfs.f_bavail = %lld\n"), (long long) st.f_bavail);
 		printf(_("statfs.f_files = %lld\n"), (long long) st.f_files);
 		printf(_("statfs.f_ffree = %lld\n"), (long long) st.f_ffree);
-		printf(_("statfs.f_fflags = 0x%llx\n"), (long long) st.f_flags);
+		printf(_("statfs.f_flags = 0x%llx\n"), (long long) st.f_flags);
 	}
 	if (file->flags & IO_FOREIGN)
 		return 0;