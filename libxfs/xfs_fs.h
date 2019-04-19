kernelorg-mirror/linux_kernel_git_djwong_xfsprogs-dev

xfs: report AG health via AG geometry ioctl
Use the AG geometry info ioctl to report health status too.

Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

libxfs/xfs_fs.h
@@ -305,9 +305,19 @@ struct xfs_ag_geometry {
 	__u32		ag_freeblks;	/* o: free space */
 	__u32		ag_icount;	/* o: inodes allocated */
 	__u32		ag_ifree;	/* o: inodes free */
-	__u32		ag_reserved32;	/* o: zero */
+	__u32		ag_health;	/* o: sick things in ag */
 	__u64		ag_reserved[5];	/* o: zero */
 };
+#define XFS_AG_GEOM_HEALTH_AG_SB	(1 << 0)  /* superblock */
+#define XFS_AG_GEOM_HEALTH_AG_AGF	(1 << 1)  /* AGF header */
+#define XFS_AG_GEOM_HEALTH_AG_AGFL	(1 << 2)  /* AGFL header */
+#define XFS_AG_GEOM_HEALTH_AG_AGI	(1 << 3)  /* AGI header */
+#define XFS_AG_GEOM_HEALTH_AG_BNOBT	(1 << 4)  /* free space by block */
+#define XFS_AG_GEOM_HEALTH_AG_CNTBT	(1 << 5)  /* free space by length */
+#define XFS_AG_GEOM_HEALTH_AG_INOBT	(1 << 6)  /* inode index */
+#define XFS_AG_GEOM_HEALTH_AG_FINOBT	(1 << 7)  /* free inode index */
+#define XFS_AG_GEOM_HEALTH_AG_RMAPBT	(1 << 8)  /* reverse mappings */
+#define XFS_AG_GEOM_HEALTH_AG_REFCNTBT	(1 << 9)  /* reference counts */
 
 /*
  * Structures for XFS_IOC_FSGROWFSDATA, XFS_IOC_FSGROWFSLOG & XFS_IOC_FSGROWFSRT

johnpgarry/xfsprogs-dev

libxfs/xfs_fs.h
@@ -306,9 +306,21 @@ struct xfs_ag_geometry {
 	uint32_t	ag_freeblks;	/* o: free space */
 	uint32_t	ag_icount;	/* o: inodes allocated */
 	uint32_t	ag_ifree;	/* o: inodes free */
+	uint32_t	ag_sick;	/* o: sick things in ag */
+	uint32_t	ag_checked;	/* o: checked metadata in ag */
 	uint32_t	ag_reserved32;	/* o: zero */
-	uint64_t	ag_reserved[13];/* o: zero */
+	uint64_t	ag_reserved[12];/* o: zero */
 };
+#define XFS_AG_GEOM_SICK_SB	(1 << 0)  /* superblock */
+#define XFS_AG_GEOM_SICK_AGF	(1 << 1)  /* AGF header */
+#define XFS_AG_GEOM_SICK_AGFL	(1 << 2)  /* AGFL header */
+#define XFS_AG_GEOM_SICK_AGI	(1 << 3)  /* AGI header */
+#define XFS_AG_GEOM_SICK_BNOBT	(1 << 4)  /* free space by block */
+#define XFS_AG_GEOM_SICK_CNTBT	(1 << 5)  /* free space by length */
+#define XFS_AG_GEOM_SICK_INOBT	(1 << 6)  /* inode index */
+#define XFS_AG_GEOM_SICK_FINOBT	(1 << 7)  /* free inode index */
+#define XFS_AG_GEOM_SICK_RMAPBT	(1 << 8)  /* reverse mappings */
+#define XFS_AG_GEOM_SICK_REFCNTBT (1 << 9)  /* reference counts */
 
 /*
  * Structures for XFS_IOC_FSGROWFSDATA, XFS_IOC_FSGROWFSLOG & XFS_IOC_FSGROWFSRT