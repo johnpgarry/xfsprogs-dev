a743edab267d3ffa5a7b7d82873428a43d06fb44
Darrick J. Wong <darrick.wong@oracle.com>
2019-06-25 14:57:56 -0700

    xfs_spaceman: remove typedef usage
    Kill off the struct typedefs inside spaceman.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- spaceman/space.h
+++ spaceman/space.h
@@ -6,16 +6,16 @@
 #ifndef XFS_SPACEMAN_SPACE_H_
 #define XFS_SPACEMAN_SPACE_H_
 
-typedef struct fileio {
+struct fileio {
 	struct xfs_fsop_geom geom;		/* XFS filesystem geometry */
 	struct fs_path	fs_path;	/* XFS path information */
 	char		*name;		/* file name at time of open */
 	int		fd;		/* open file descriptor */
-} fileio_t;
+};
 
-extern fileio_t		*filetable;	/* open file table */
+extern struct fileio	*filetable;	/* open file table */
 extern int		filecount;	/* number of open files */
-extern fileio_t		*file;		/* active file in file table */
+extern struct fileio	*file;		/* active file in file table */
 
 extern int	openfile(char *, struct xfs_fsop_geom *, struct fs_path *);
 extern int	addfile(char *, int , struct xfs_fsop_geom *, struct fs_path *);
