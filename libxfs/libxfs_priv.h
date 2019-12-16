a757fe94778c1f29fcc0f8b104434c905beebb4b
Christoph Hellwig <hch@lst.de>
2019-12-16 09:34:58 -0800

    xfs: cleanup xfs_dir2_data_entsize
    Source kernel commit: 711c7dbf5fdafdbb2fb906b38cefb113bfdd4a79
    
    Remove the XFS_DIR2_DATA_ENTSIZE and XFS_DIR3_DATA_ENTSIZE and open
    code them in their only caller, which now becomes so simple that
    we can turn it into an inline function.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Darrick J. Wong <darrick.wong@oracle.com>
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- libxfs/libxfs_priv.h
+++ libxfs/libxfs_priv.h
@@ -263,11 +263,6 @@ div_u64_rem(uint64_t dividend, uint32_t divisor, uint32_t *remainder)
 #define swap(a, b) \
 	do { typeof(a) __tmp = (a); (a) = (b); (b) = __tmp; } while (0)
 
-#define __round_mask(x, y) ((__typeof__(x))((y)-1))
-#define round_up(x, y) ((((x)-1) | __round_mask(x, y))+1)
-#define round_down(x, y) ((x) & ~__round_mask(x, y))
-#define DIV_ROUND_UP(n,d) (((n) + (d) - 1) / (d))
-
 /*
  * Handling for kernel bitmap types.
  */
