bd045c51e45667e02e81670e8ff0fc6ae976f048
Christoph Hellwig <hch@lst.de>
2020-01-22 11:29:41 -0500

    xfs: move incore structures out of xfs_da_format.h
    Source kernel commit: a39f089a25e75c3d17b955d8eb8bc781f23364f3
    
    Move the abstract in-memory version of various btree block headers
    out of xfs_da_format.h as they aren't on-disk formats.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Darrick J. Wong <darrick.wong@oracle.com>
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>
    Signed-off-by: Eric Sandeen <sandeen@sandeen.net>

--- include/libxfs.h
+++ include/libxfs.h
@@ -52,6 +52,7 @@ struct iomap;
 #include "xfs_da_format.h"
 #include "xfs_da_btree.h"
 #include "xfs_dir2.h"
+#include "xfs_dir2_priv.h"
 #include "xfs_bmap_btree.h"
 #include "xfs_alloc_btree.h"
 #include "xfs_ialloc_btree.h"
