0a664190fc8501ae9d7b72f8a9bf98e424759114
Christoph Hellwig <hch@lst.de>
2020-06-04 09:52:20 -0700

    xfs: use a xfs_btree_cur for the ->finish_cleanup state
    Source kernel commit: 94c06c450591bd85d83838ebefe71537077950e1
    
    Given how XFS is all based around btrees it doesn't make much sense
    to offer a totally generic state when we can just use the btree cursor.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Brian Foster <bfoster@redhat.com>
    Reviewed-by: Darrick J. Wong <darrick.wong@oracle.com>
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- libxfs/defer_item.c
+++ libxfs/defer_item.c
@@ -52,7 +52,7 @@ xfs_extent_free_finish_item(
 	struct xfs_trans		*tp,
 	struct xfs_log_item		*done,
 	struct list_head		*item,
-	void				**state)
+	struct xfs_btree_cur		**state)
 {
 	struct xfs_extent_free_item	*free;
 	int				error;
@@ -100,7 +100,7 @@ xfs_agfl_free_finish_item(
 	struct xfs_trans		*tp,
 	struct xfs_log_item		*done,
 	struct list_head		*item,
-	void				**state)
+	struct xfs_btree_cur		**state)
 {
 	struct xfs_mount		*mp = tp->t_mountp;
 	struct xfs_extent_free_item	*free;
@@ -160,7 +160,7 @@ xfs_rmap_update_finish_item(
 	struct xfs_trans		*tp,
 	struct xfs_log_item		*done,
 	struct list_head		*item,
-	void				**state)
+	struct xfs_btree_cur		**state)
 {
 	struct xfs_rmap_intent		*rmap;
 	int				error;
@@ -173,7 +173,7 @@ xfs_rmap_update_finish_item(
 			rmap->ri_bmap.br_startblock,
 			rmap->ri_bmap.br_blockcount,
 			rmap->ri_bmap.br_state,
-			(struct xfs_btree_cur **)state);
+			state);
 	kmem_free(rmap);
 	return error;
 }
@@ -182,11 +182,9 @@ xfs_rmap_update_finish_item(
 STATIC void
 xfs_rmap_update_finish_cleanup(
 	struct xfs_trans	*tp,
-	void			*state,
+	struct xfs_btree_cur	*rcur,
 	int			error)
 {
-	struct xfs_btree_cur	*rcur = state;
-
 	xfs_rmap_finish_one_cleanup(tp, rcur, error);
 }
 
@@ -246,7 +244,7 @@ xfs_refcount_update_finish_item(
 	struct xfs_trans		*tp,
 	struct xfs_log_item		*done,
 	struct list_head		*item,
-	void				**state)
+	struct xfs_btree_cur		**state)
 {
 	struct xfs_refcount_intent	*refc;
 	xfs_fsblock_t			new_fsb;
@@ -259,7 +257,7 @@ xfs_refcount_update_finish_item(
 			refc->ri_startblock,
 			refc->ri_blockcount,
 			&new_fsb, &new_aglen,
-			(struct xfs_btree_cur **)state);
+			state);
 	/* Did we run out of reservation?  Requeue what we didn't finish. */
 	if (!error && new_aglen > 0) {
 		ASSERT(refc->ri_type == XFS_REFCOUNT_INCREASE ||
@@ -276,11 +274,9 @@ xfs_refcount_update_finish_item(
 STATIC void
 xfs_refcount_update_finish_cleanup(
 	struct xfs_trans	*tp,
-	void			*state,
+	struct xfs_btree_cur	*rcur,
 	int			error)
 {
-	struct xfs_btree_cur	*rcur = state;
-
 	xfs_refcount_finish_one_cleanup(tp, rcur, error);
 }
 
@@ -340,7 +336,7 @@ xfs_bmap_update_finish_item(
 	struct xfs_trans		*tp,
 	struct xfs_log_item		*done,
 	struct list_head		*item,
-	void				**state)
+	struct xfs_btree_cur		**state)
 {
 	struct xfs_bmap_intent		*bmap;
 	xfs_filblks_t			count;
