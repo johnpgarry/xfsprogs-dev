mtanski/xfsprogs

commit 2556c98bd9e6b2222ff653ec1af4deca5f4da4f3

Perform true sequential bulk read prefetching in xfs_repair Merge of master-melb:xfs-cmds:29147a by kenmcd.
  Perform parallel sequential bulk read prefetching in xfs_repair

repair/dinode.c
@@ -514,28 +514,121 @@ get_bmbt_reclist(
 	return(NULLDFSBNO);
 }
 
-/*
- * process_bmbt_reclist_int is the most compute intensive
- * function in repair. The following macros reduce the
- * the large number of lock/unlock steps it would otherwise
- * call.
- */
-#define	PROCESS_BMBT_DECL(type, var)	type var
 
-#define	PROCESS_BMBT_LOCK(agno)							\
-	if (do_parallel && (agno != locked_agno)) {				\
-		if (locked_agno != -1)	/* release old ag lock */		\
-			PREPAIR_RW_UNLOCK_NOTEST(&per_ag_lock[locked_agno]);	\
-		PREPAIR_RW_WRITE_LOCK_NOTEST(&per_ag_lock[agno]);		\
-		locked_agno = agno;						\
+static int
+process_rt_rec(
+	xfs_mount_t		*mp,
+	xfs_bmbt_rec_32_t	*rp,
+	xfs_ino_t		ino,
+	xfs_drfsbno_t		*tot,
+	int			check_dups)
+{
+	xfs_dfsbno_t		b;
+	xfs_drtbno_t		ext;
+	xfs_dfilblks_t		c;		/* count */
+	xfs_dfsbno_t		s;		/* start */
+	xfs_dfiloff_t		o;		/* offset */
+	int			state;
+	int			flag;		/* extent flag */
+	int			pwe;		/* partially-written extent */
+
+	convert_extent(rp, &o, &s, &c, &flag);
+
+	/*
+	 * check numeric validity of the extent
+	 */
+	if (s >= mp->m_sb.sb_rblocks)  {
+		do_warn(_("inode %llu - bad rt extent start block number "
+				"%llu, offset %llu\n"), ino, s, o);
+		return 1;
+	}
+	if (s + c - 1 >= mp->m_sb.sb_rblocks)  {
+		do_warn(_("inode %llu - bad rt extent last block number %llu, "
+				"offset %llu\n"), ino, s + c - 1, o);
+		return 1;
+	}
+	if (s + c - 1 < s)  {
+		do_warn(_("inode %llu - bad rt extent overflows - start %llu, "
+				"end %llu, offset %llu\n"),
+				ino, s, s + c - 1, o);
+		return 1;
 	}
 
-#define	PROCESS_BMBT_UNLOCK_RETURN(val)						\
-	do {									\
-		if (locked_agno != -1) 						\
-			PREPAIR_RW_UNLOCK_NOTEST(&per_ag_lock[locked_agno]);	\
-		return (val);							\
-	} while (0)
+	/*
+	 * verify that the blocks listed in the record
+	 * are multiples of an extent
+	 */
+	if (XFS_SB_VERSION_HASEXTFLGBIT(&mp->m_sb) == 0 &&
+			(s % mp->m_sb.sb_rextsize != 0 ||
+			 c % mp->m_sb.sb_rextsize != 0)) {
+		do_warn(_("malformed rt inode extent [%llu %llu] (fs rtext "
+				"size = %u)\n"), s, c, mp->m_sb.sb_rextsize);
+		return 1;
+	}
+
+	/*
+	 * set the appropriate number of extents
+	 */
+	for (b = s; b < s + c; b += mp->m_sb.sb_rextsize)  {
+		ext = (xfs_drtbno_t) b / mp->m_sb.sb_rextsize;
+		pwe = XFS_SB_VERSION_HASEXTFLGBIT(&mp->m_sb) && flag &&
+				(b % mp->m_sb.sb_rextsize != 0);
+
+		if (check_dups == 1)  {
+			if (search_rt_dup_extent(mp, ext) && !pwe)  {
+				do_warn(_("data fork in rt ino %llu claims "
+						"dup rt extent, off - %llu, "
+						"start - %llu, count %llu\n"),
+						ino, o, s, c);
+				return 1;
+			}
+			continue;
+		}
+
+		state = get_rtbno_state(mp, ext);
+
+		switch (state)  {
+			case XR_E_FREE:
+			case XR_E_UNKNOWN:
+				set_rtbno_state(mp, ext, XR_E_INUSE);
+				break;
+
+			case XR_E_BAD_STATE:
+				do_error(_("bad state in rt block map %llu\n"),
+						ext);
+
+			case XR_E_FS_MAP:
+			case XR_E_INO:
+			case XR_E_INUSE_FS:
+				do_error(_("data fork in rt inode %llu found "
+						"metadata block %llu in rt bmap\n"),
+						ino, ext);
+
+			case XR_E_INUSE:
+				if (pwe)
+					break;
+
+			case XR_E_MULT:
+				set_rtbno_state(mp, ext, XR_E_MULT);
+				do_warn(_("data fork in rt inode %llu claims "
+						"used rt block %llu\n"),
+						ino, ext);
+				return 1;
+
+			case XR_E_FREE1:
+			default:
+				do_error(_("illegal state %d in rt block map "
+						"%llu\n"), state, b);
+		}
+	}
+
+	/*
+	 * bump up the block counter
+	 */
+	*tot += c;
+
+	return 0;
+}
 
 /*
  * return 1 if inode should be cleared, 0 otherwise
@@ -560,7 +653,6 @@ process_bmbt_reclist_int(
 	int			whichfork)
 {
 	xfs_dfsbno_t		b;
-	xfs_drtbno_t		ext;
 	xfs_dfilblks_t		c;		/* count */
 	xfs_dfilblks_t		cp = 0;		/* prev count */
 	xfs_dfsbno_t		s;		/* start */
@@ -572,12 +664,11 @@ process_bmbt_reclist_int(
 	int			i;
 	int			state;
 	int			flag;		/* extent flag */
-	int			pwe;		/* partially-written extent */
 	xfs_dfsbno_t		e;
 	xfs_agnumber_t		agno;
 	xfs_agblock_t		agbno;
-	PROCESS_BMBT_DECL
-				(xfs_agnumber_t, locked_agno=-1);
+	xfs_agnumber_t		locked_agno = -1;
+	int			error = 1;
 
 	if (whichfork == XFS_DATA_FORK)
 		forkname = _("data");
@@ -596,11 +687,10 @@ process_bmbt_reclist_int(
 		else
 			*last_key = o;
 		if (i > 0 && op + cp > o)  {
-			do_warn(
-	_("bmap rec out of order, inode %llu entry %d "
-	  "[o s c] [%llu %llu %llu], %d [%llu %llu %llu]\n"),
+			do_warn(_("bmap rec out of order, inode %llu entry %d "
+	  			"[o s c] [%llu %llu %llu], %d [%llu %llu %llu]\n"),
 				ino, i, o, s, c, i-1, op, sp, cp);
-			PROCESS_BMBT_UNLOCK_RETURN(1);
+			goto done;
 		}
 		op = o;
 		cp = c;
@@ -610,150 +700,18 @@ process_bmbt_reclist_int(
 		 * check numeric validity of the extent
 		 */
 		if (c == 0)  {
-			do_warn(
-	_("zero length extent (off = %llu, fsbno = %llu) in ino %llu\n"),
-				o, s, ino);
-			PROCESS_BMBT_UNLOCK_RETURN(1);
-		}
-		if (type == XR_INO_RTDATA) {
-			if (s >= mp->m_sb.sb_rblocks)  {
-				do_warn(
-	_("inode %llu - bad rt extent start block number %llu, offset %llu\n"),
-					ino, s, o);
-				PROCESS_BMBT_UNLOCK_RETURN(1);
-			}
-			if (s + c - 1 >= mp->m_sb.sb_rblocks)  {
-				do_warn(
-	_("inode %llu - bad rt extent last block number %llu, offset %llu\n"),
-					ino, s + c - 1, o);
-				PROCESS_BMBT_UNLOCK_RETURN(1);
-			}
-			if (s + c - 1 < s)  {
-				do_warn(
-	_("inode %llu - bad rt extent overflows - start %llu, end %llu, "
-	  "offset %llu\n"),
-					ino, s, s + c - 1, o);
-				PROCESS_BMBT_UNLOCK_RETURN(1);
-			}
-		} else  {
-			switch (verify_dfsbno_range(mp, s, c)) {
-			case XR_DFSBNORANGE_VALID:
-				break;
-			case XR_DFSBNORANGE_BADSTART:
-				do_warn(
-	_("inode %llu - bad extent starting block number %llu, offset %llu\n"),
-					ino, s, o);
-				PROCESS_BMBT_UNLOCK_RETURN(1);
-			case XR_DFSBNORANGE_BADEND:
-				do_warn(
-	_("inode %llu - bad extent last block number %llu, offset %llu\n"),
-					ino, s + c - 1, o);
-				PROCESS_BMBT_UNLOCK_RETURN(1);
-			case XR_DFSBNORANGE_OVERFLOW:
-				do_warn(
-
-	_("inode %llu - bad extent overflows - start %llu, end %llu, "
-	  "offset %llu\n"),
-					ino, s, s + c - 1, o);
-				PROCESS_BMBT_UNLOCK_RETURN(1);
-			}
-			if (o >= fs_max_file_offset)  {
-				do_warn(
-	_("inode %llu - extent offset too large - start %llu, count %llu, "
-	  "offset %llu\n"),
-					ino, s, c, o);
-				PROCESS_BMBT_UNLOCK_RETURN(1);
-			}
+			do_warn(_("zero length extent (off = %llu, "
+				"fsbno = %llu) in ino %llu\n"), o, s, ino);
+			goto done;
 		}
 
-		/*
-		 * realtime file data fork
-		 */
-		if (type == XR_INO_RTDATA && whichfork == XFS_DATA_FORK)  {
+		if (type == XR_INO_RTDATA && whichfork == XFS_DATA_FORK) {
 			/*
-			 * XXX - verify that the blocks listed in the record
-			 * are multiples of an extent
+			 * realtime bitmaps don't use AG locks, so returning
+			 * immediately is fine for this code path.
 			 */
-			if (XFS_SB_VERSION_HASEXTFLGBIT(&mp->m_sb) == 0
-			   && (s % mp->m_sb.sb_rextsize != 0 ||
-					c % mp->m_sb.sb_rextsize != 0))  {
-				do_warn(
-	_("malformed rt inode extent [%llu %llu] (fs rtext size = %u)\n"),
-					s, c, mp->m_sb.sb_rextsize);
-				PROCESS_BMBT_UNLOCK_RETURN(1);
-			}
-
-			/*
-			 * XXX - set the appropriate number of extents
-			 */
-			for (b = s; b < s + c; b += mp->m_sb.sb_rextsize)  {
-				ext = (xfs_drtbno_t) b / mp->m_sb.sb_rextsize;
-				if (XFS_SB_VERSION_HASEXTFLGBIT(&mp->m_sb) &&
-				    flag && (b % mp->m_sb.sb_rextsize != 0)) {
-					pwe = 1;
-				} else {
-					pwe = 0;
-				}
-
-				if (check_dups == 1)  {
-					if (search_rt_dup_extent(mp, ext) &&
-					    !pwe)  {
-						do_warn(
-	_("data fork in rt ino %llu claims dup rt extent, off - %llu, "
-	  "start - %llu, count %llu\n"),
-							ino, o, s, c);
-						PROCESS_BMBT_UNLOCK_RETURN(1);
-					}
-					continue;
-				}
-
-				state = get_rtbno_state(mp, ext);
-
-				switch (state)  {
-				case XR_E_FREE:
-/* XXX - turn this back on after we
-	run process_rtbitmap() in phase2
-					do_warn(
-			_("%s fork in rt ino %llu claims free rt block %llu\n"),
-						forkname, ino, ext);
-*/
-					/* fall through ... */
-				case XR_E_UNKNOWN:
-					set_rtbno_state(mp, ext, XR_E_INUSE);
-					break;
-				case XR_E_BAD_STATE:
-					do_error(
-				_("bad state in rt block map %llu\n"), ext);
-					abort();
-					break;
-				case XR_E_FS_MAP:
-				case XR_E_INO:
-				case XR_E_INUSE_FS:
-					do_error(
-	_("%s fork in rt inode %llu found metadata block %llu in %s bmap\n"),
-						forkname, ino, ext, ftype);
-				case XR_E_INUSE:
-					if (pwe)
-						break;
-				case XR_E_MULT:
-					set_rtbno_state(mp, ext, XR_E_MULT);
-					do_warn(
-	_("%s fork in rt inode %llu claims used rt block %llu\n"),
-						forkname, ino, ext);
-					PROCESS_BMBT_UNLOCK_RETURN(1);
-				case XR_E_FREE1:
-				default:
-					do_error(
-				_("illegal state %d in %s block map %llu\n"),
-						state, ftype, b);
-				}
-			}
-
-			/*
-			 * bump up the block counter
-			 */
-			*tot += c;
-
+			if (process_rt_rec(mp, rp, ino, tot, check_dups))
+				return 1;
 			/*
 			 * skip rest of loop processing since that's
 			 * all for regular file forks and attr forks
@@ -761,10 +719,38 @@ process_bmbt_reclist_int(
 			continue;
 		}
 
-
 		/*
 		 * regular file data fork or attribute fork
 		 */
+		switch (verify_dfsbno_range(mp, s, c)) {
+			case XR_DFSBNORANGE_VALID:
+				break;
+
+			case XR_DFSBNORANGE_BADSTART:
+				do_warn(_("inode %llu - bad extent starting "
+					"block number %llu, offset %llu\n"),
+					ino, s, o);
+				goto done;
+
+			case XR_DFSBNORANGE_BADEND:
+				do_warn(_("inode %llu - bad extent last block "
+					"number %llu, offset %llu\n"),
+					ino, s + c - 1, o);
+				goto done;
+
+			case XR_DFSBNORANGE_OVERFLOW:
+				do_warn(_("inode %llu - bad extent overflows - "
+					"start %llu, end %llu, offset %llu\n"),
+					ino, s, s + c - 1, o);
+				goto done;
+		}
+		if (o >= fs_max_file_offset)  {
+			do_warn(_("inode %llu - extent offset too large - "
+				"start %llu, count %llu, offset %llu\n"),
+				ino, s, c, o);
+			goto done;
+		}
+
 		if (blkmapp && *blkmapp)
 			blkmap_set_ext(blkmapp, o, s, c);
 		/*
@@ -774,35 +760,36 @@ process_bmbt_reclist_int(
 		agno = XFS_FSB_TO_AGNO(mp, s);
 		agbno = XFS_FSB_TO_AGBNO(mp, s);
 		e = s + c;
-		PROCESS_BMBT_LOCK(agno);
-		for (b = s; b < e; b++, agbno++)  {
-			if (check_dups == 1)  {
-				/*
-				 * if we're just checking the bmap for dups,
-				 * return if we find one, otherwise, continue
-				 * checking each entry without setting the
-				 * block bitmap
-				 */
-				if (search_dup_extent(mp, agno, agbno)) {
-					do_warn(
-	_("%s fork in ino %llu claims dup extent, off - %llu, "
-	  "start - %llu, cnt %llu\n"),
-						forkname, ino, o, s, c);
-					PROCESS_BMBT_UNLOCK_RETURN(1);
-				}
-				continue;
-			}
+		if (agno != locked_agno) {
+			if (locked_agno != -1)
+				pthread_mutex_unlock(&ag_locks[locked_agno]);
+			pthread_mutex_lock(&ag_locks[agno]);
+			locked_agno = agno;
+		}
 
-			/* FIX FOR BUG 653709 -- EKN
-			 * realtime attribute fork, should be valid block number
-			 * in regular data space, not realtime partion.
+		if (check_dups) {
+			/*
+			 * if we're just checking the bmap for dups,
+			 * return if we find one, otherwise, continue
+			 * checking each entry without setting the
+			 * block bitmap
 			 */
-			if (type == XR_INO_RTDATA && whichfork == XFS_ATTR_FORK) {
-			  if (mp->m_sb.sb_agcount < agno)
-				PROCESS_BMBT_UNLOCK_RETURN(1);
+			for (b = s; b < e; b++, agbno++)  {
+				if (search_dup_extent(mp, agno, agbno)) {
+					do_warn(_("%s fork in ino %llu claims "
+						"dup extent, off - %llu, "
+						"start - %llu, cnt %llu\n"),
+						forkname, ino, o, s, c);
+					goto done;
+				}
 			}
+			*tot += c;
+			continue;
+		}
 
-			/* Process in chunks of 16 (XR_BB_UNIT/XR_BB)
+		for (b = s; b < e; b++, agbno++)  {
+			/*
+			 * Process in chunks of 16 (XR_BB_UNIT/XR_BB)
 			 * for common XR_E_UNKNOWN to XR_E_INUSE transition
 			 */
 			if (((agbno & XR_BB_MASK) == 0) && ((s + c - b) >= (XR_BB_UNIT/XR_BB))) {
@@ -816,45 +803,49 @@ process_bmbt_reclist_int(
 			}
 
 			state = get_agbno_state(mp, agno, agbno);
+
 			switch (state)  {
 			case XR_E_FREE:
 			case XR_E_FREE1:
-				do_warn(
-			_("%s fork in ino %llu claims free block %llu\n"),
+				do_warn(_("%s fork in ino %llu claims free "
+					"block %llu\n"),
 					forkname, ino, (__uint64_t) b);
 				/* fall through ... */
 			case XR_E_UNKNOWN:
 				set_agbno_state(mp, agno, agbno, XR_E_INUSE);
 				break;
+
 			case XR_E_BAD_STATE:
 				do_error(_("bad state in block map %llu\n"), b);
-				abort();
-				break;
+
 			case XR_E_FS_MAP:
 			case XR_E_INO:
 			case XR_E_INUSE_FS:
-				do_warn(
-			_("%s fork in inode %llu claims metadata block %llu\n"),
+				do_warn(_("%s fork in inode %llu claims "
+					"metadata block %llu\n"),
 					forkname, ino, (__uint64_t) b);
-				PROCESS_BMBT_UNLOCK_RETURN(1);
+				goto done;
+
 			case XR_E_INUSE:
 			case XR_E_MULT:
 				set_agbno_state(mp, agno, agbno, XR_E_MULT);
-				do_warn(
-			_("%s fork in %s inode %llu claims used block %llu\n"),
+				do_warn(_("%s fork in %s inode %llu claims "
+					"used block %llu\n"),
 					forkname, ftype, ino, (__uint64_t) b);
-				PROCESS_BMBT_UNLOCK_RETURN(1);
+				goto done;
+
 			default:
-				do_error(
-			_("illegal state %d in block map %llu\n"),
+				do_error(_("illegal state %d in block map %llu\n"),
 					state, b);
-				abort();
 			}
 		}
 		*tot += c;
 	}
-
-	PROCESS_BMBT_UNLOCK_RETURN(0);
+	error = 0;
+done:
+	if (locked_agno != -1)
+		pthread_mutex_unlock(&ag_locks[locked_agno]);
+	return error;
 }
 
 /*

repair/globals.h
@@ -148,7 +148,7 @@ EXTERN xfs_agblock_t	inobt_root;
 /* configuration vars -- fs geometry dependent */
 
 EXTERN int		inodes_per_block;
-EXTERN int		inodes_per_cluster;	/* inodes per inode buffer */
+EXTERN int		inodes_per_cluster;
 EXTERN unsigned int	glob_agcount;
 EXTERN int		chunks_pblock;	/* # of 64-ino chunks per allocation */
 EXTERN int		max_symlink_blocks;
@@ -192,11 +192,16 @@ extern size_t 		ts_dirbuf_size;
 extern size_t 		ts_dir_freemap_size;
 extern size_t 		ts_attr_freemap_size;
 
-EXTERN pthread_rwlock_t	*per_ag_lock;
+EXTERN pthread_mutex_t	*ag_locks;
 
-EXTERN int report_interval;
-EXTERN __uint64_t *prog_rpt_done;
+EXTERN int 		report_interval;
+EXTERN __uint64_t 	*prog_rpt_done;
+
+#ifdef XR_PF_TRACE
+EXTERN FILE		*pf_trace_file;
+#endif
 
 EXTERN int		ag_stride;
+EXTERN int		thread_count;
 
 #endif /* _XFS_REPAIR_GLOBAL_H */

repair/incore.c
@@ -61,11 +61,12 @@ setup_bmap(xfs_agnumber_t agno, xfs_agblock_t numblocks, xfs_drtbno_t rtblocks)
 	size_t size = 0;
 
 	ba_bmap = (__uint64_t**)malloc(agno*sizeof(__uint64_t *));
-	if (!ba_bmap)  {
+	if (!ba_bmap)
 		do_error(_("couldn't allocate block map pointers\n"));
-		return;
-	}
-	PREPAIR_RW_LOCK_ALLOC(per_ag_lock, agno);
+	ag_locks = malloc(agno * sizeof(pthread_mutex_t));
+	if (!ag_locks)
+		do_error(_("couldn't allocate block map locks\n"));
+
 	for (i = 0; i < agno; i++)  {
 		size = roundup((numblocks+(NBBY/XR_BB)-1) / (NBBY/XR_BB),
 		       		sizeof(__uint64_t));
@@ -77,7 +78,7 @@ setup_bmap(xfs_agnumber_t agno, xfs_agblock_t numblocks, xfs_drtbno_t rtblocks)
 			return;
 		}
 		bzero(ba_bmap[i], size);
-		PREPAIR_RW_LOCK_INIT(&per_ag_lock[i], NULL);
+		pthread_mutex_init(&ag_locks[i], NULL);
 	}
 
 	if (rtblocks == 0)  {

include/cache.h
@@ -18,6 +18,8 @@
 #ifndef __CACHE_H__
 #define __CACHE_H__
 
+#define	HASH_CACHE_RATIO	8
+
 /*
  * Simple, generic implementation of a cache (arbitrary data).
  * Provides a hash table with a capped number of cache entries.
@@ -28,8 +30,9 @@ struct cache_hash;
 struct cache_node;
 
 typedef void *cache_key_t;
+
 typedef void (*cache_walk_t)(struct cache_node *);
-typedef struct cache_node * (*cache_node_alloc_t)(void);
+typedef struct cache_node * (*cache_node_alloc_t)(cache_key_t);
 typedef void (*cache_node_flush_t)(struct cache_node *);
 typedef void (*cache_node_relse_t)(struct cache_node *);
 typedef unsigned int (*cache_node_hash_t)(cache_key_t, unsigned int);
@@ -84,5 +87,6 @@ int cache_node_get(struct cache *, cache_key_t, struct cache_node **);
 void cache_node_put(struct cache_node *);
 int cache_node_purge(struct cache *, cache_key_t, struct cache_node *);
 void cache_report(FILE *fp, const char *, struct cache *);
+int cache_overflowed(struct cache *);
 
 #endif	/* __CACHE_H__ */

include/libxfs.h
@@ -190,8 +190,8 @@ typedef struct xfs_mount {
 #define LIBXFS_MOUNT_32BITINOOPT	0x0008
 #define LIBXFS_MOUNT_COMPAT_ATTR	0x0010
 
-#define LIBXFS_IHASHSIZE(sbp)		(1<<16)	/* tweak based on icount? */
-#define LIBXFS_BHASHSIZE(sbp)		(1<<16)	/* ditto, on blocks used? */
+#define LIBXFS_IHASHSIZE(sbp)		(1<<10)
+#define LIBXFS_BHASHSIZE(sbp) 		(1<<10)
 
 extern xfs_mount_t	*libxfs_mount (xfs_mount_t *, xfs_sb_t *,
 				dev_t, dev_t, dev_t, int);
@@ -216,10 +216,17 @@ typedef struct xfs_buf {
 	xfs_daddr_t		b_blkno;
 	unsigned		b_bcount;
 	dev_t			b_dev;
+	pthread_mutex_t		b_lock;
 	void			*b_fsprivate;
 	void			*b_fsprivate2;
 	void			*b_fsprivate3;
 	char			*b_addr;
+#ifdef XFS_BUF_TRACING
+	struct list_head	b_lock_list;
+	const char		*b_func;
+	const char		*b_file;
+	int			b_line;
+#endif
 } xfs_buf_t;
 
 enum xfs_buf_flags_t {	/* b_flags bits */
@@ -247,25 +254,49 @@ enum xfs_buf_flags_t {	/* b_flags bits */
 #define XFS_BUF_FSPRIVATE3(bp,type)	((type)(bp)->b_fsprivate3)
 #define XFS_BUF_SET_FSPRIVATE3(bp,val)	(bp)->b_fsprivate3 = (void *)(val)
 
-extern xfs_buf_t	*libxfs_getsb (xfs_mount_t *, int);
-extern xfs_buf_t	*libxfs_readbuf (dev_t, xfs_daddr_t, int, int);
-extern int	libxfs_readbufr (dev_t, xfs_daddr_t, xfs_buf_t *, int, int);
-extern int	libxfs_writebuf (xfs_buf_t *, int);
-extern int	libxfs_writebufr (xfs_buf_t *);
-extern int	libxfs_writebuf_int (xfs_buf_t *, int);
-
 /* Buffer Cache Interfaces */
+
 extern struct cache	*libxfs_bcache;
 extern struct cache_operations	libxfs_bcache_operations;
-extern void	libxfs_bcache_purge (void);
-extern void	libxfs_bcache_flush (void);
-extern xfs_buf_t	*libxfs_getbuf (dev_t, xfs_daddr_t, int);
+
+#ifdef XFS_BUF_TRACING
+
+#define libxfs_readbuf(dev, daddr, len, flags) \
+		libxfs_trace_readbuf(__FUNCTION__, __FILE__, __LINE__, (dev), (daddr), (len), (flags))
+#define libxfs_writebuf(buf, flags) \
+		libxfs_trace_writebuf(__FUNCTION__, __FILE__, __LINE__, (buf), (flags))
+#define libxfs_getbuf(dev, daddr, len) \
+		libxfs_trace_getbuf(__FUNCTION__, __FILE__, __LINE__, (dev), (daddr), (len))
+#define libxfs_putbuf(buf) \
+		libxfs_trace_putbuf(__FUNCTION__, __FILE__, __LINE__, (buf))
+
+extern xfs_buf_t *libxfs_trace_readbuf(const char *, const char *, int, dev_t, xfs_daddr_t, int, int);
+extern int	libxfs_trace_writebuf(const char *, const char *, int, xfs_buf_t *, int);
+extern xfs_buf_t *libxfs_trace_getbuf(const char *, const char *, int, dev_t, xfs_daddr_t, int);
+extern void	libxfs_trace_putbuf (const char *, const char *, int, xfs_buf_t *);
+
+#else
+
+extern xfs_buf_t *libxfs_readbuf(dev_t, xfs_daddr_t, int, int);
+extern int	libxfs_writebuf(xfs_buf_t *, int);
+extern xfs_buf_t *libxfs_getbuf(dev_t, xfs_daddr_t, int);
 extern void	libxfs_putbuf (xfs_buf_t *);
-extern void	libxfs_purgebuf (xfs_buf_t *);
+
+#endif
+
+extern xfs_buf_t *libxfs_getsb(xfs_mount_t *, int);
+extern void	libxfs_bcache_purge(void);
+extern void	libxfs_bcache_flush(void);
+extern void	libxfs_purgebuf(xfs_buf_t *);
+extern int	libxfs_bcache_overflowed(void);
+extern int	libxfs_bcache_usage(void);
 
 /* Buffer (Raw) Interfaces */
-extern xfs_buf_t	*libxfs_getbufr (dev_t, xfs_daddr_t, int);
-extern void	libxfs_putbufr (xfs_buf_t *);
+extern xfs_buf_t *libxfs_getbufr(dev_t, xfs_daddr_t, int);
+extern void	libxfs_putbufr(xfs_buf_t *);
+
+extern int	libxfs_writebuf_int(xfs_buf_t *, int);
+extern int	libxfs_readbufr(dev_t, xfs_daddr_t, xfs_buf_t *, int, int);
 
 extern int libxfs_bhash_size;
 extern int libxfs_ihash_size;
@@ -556,29 +587,10 @@ extern unsigned int	libxfs_log2_roundup(unsigned int i);
 extern void cmn_err(int, char *, ...);
 enum ce { CE_DEBUG, CE_CONT, CE_NOTE, CE_WARN, CE_ALERT, CE_PANIC };
 
-/* lio interface */
-/* lio_listio(3) interface (POSIX linked asynchronous I/O) */
-extern int libxfs_lio_ino_count;
-extern int libxfs_lio_dir_count;
-extern int libxfs_lio_aio_count;
-
-extern int libxfs_lio_init(void);
-extern void libxfs_lio_allocate(void);
-extern void *libxfs_get_lio_buffer(int type);
-extern void libxfs_put_lio_buffer(void *buffer);
-extern int libxfs_readbuf_list(dev_t dev, int nent, void *voidp, int type);
-
-typedef struct  libxfs_lio_req {
-	xfs_daddr_t	blkno;
-	int		len;	/* bbs */
-} libxfs_lio_req_t;
-
-#define	LIBXFS_LIO_TYPE_INO		0x1
-#define	LIBXFS_LIO_TYPE_DIR		0x2
-#define	LIBXFS_LIO_TYPE_RAW		0x3
 
 #define LIBXFS_BBTOOFF64(bbs)	(((xfs_off_t)(bbs)) << BBSHIFT)
-extern int libxfs_nproc(void);
+extern int		libxfs_nproc(void);
+extern unsigned long	libxfs_physmem(void);	/* in kilobytes */
 
 #include <xfs/xfs_ialloc.h>
 #include <xfs/xfs_rtalloc.h>

include/linux.h
@@ -119,9 +119,4 @@ typedef enum {B_FALSE, B_TRUE}	boolean_t;
 #define _BOOLEAN_T_DEFINED	1
 #endif
 
-#ifdef __USE_GNU
-typedef struct aiocb64 aiocb64_t;
-#define	_AIOCB64_T_DEFINED	1
-#endif
-
 #endif	/* __XFS_LINUX_H__ */

libxfs/Makefile
@@ -11,7 +11,7 @@ LT_REVISION = 0
 LT_AGE = 0
 
 HFILES = xfs.h init.h
-CFILES = bit.c cache.c init.c lio.c logitem.c rdwr.c trans.c util.c \
+CFILES = bit.c cache.c init.c logitem.c rdwr.c trans.c util.c \
 	xfs_alloc.c xfs_ialloc.c xfs_rtalloc.c \
 	xfs_inode.c xfs_btree.c xfs_alloc_btree.c xfs_ialloc_btree.c \
 	xfs_bmap_btree.c xfs_da_btree.c xfs_dir.c xfs_dir_leaf.c \

libxfs/cache.c
@@ -31,7 +31,6 @@
 #define CACHE_DEBUG 1
 #undef CACHE_ABORT
 /* #define CACHE_ABORT 1 */
-#define	HASH_CACHE_RATIO	8
 
 static unsigned int cache_generic_bulkrelse(struct cache *, struct list_head *);
 
@@ -275,7 +274,8 @@ cache_shake(
 struct cache_node *
 cache_node_allocate(
 	struct cache *		cache,
-	struct cache_hash *	hashlist)
+	struct cache_hash *	hashlist,
+	cache_key_t		key)
 {
 	unsigned int		nodesfree;
 	struct cache_node *	node;
@@ -290,7 +290,7 @@ cache_node_allocate(
 	pthread_mutex_unlock(&cache->c_mutex);
 	if (!nodesfree)
 		return NULL;
-	if (!(node = cache->alloc())) {	/* uh-oh */
+	if (!(node = cache->alloc(key))) {	/* uh-oh */
 		pthread_mutex_lock(&cache->c_mutex);
 		cache->c_count--;
 		pthread_mutex_unlock(&cache->c_mutex);
@@ -300,6 +300,13 @@ cache_node_allocate(
 	list_head_init(&node->cn_list);
 	node->cn_count = 1;
 	return node;
+}
+
+int
+cache_overflowed(
+	struct cache *		cache)
+{
+	return (cache->c_maxcount == cache->c_max);
 }
 
 /*
@@ -341,7 +348,7 @@ cache_node_get(
 		break;
 	}
 	if (pos == head) {
-		node = cache_node_allocate(cache, hash);
+		node = cache_node_allocate(cache, hash, key);
 		if (!node) {
 			priority = cache_shake(cache, hash, priority);
 			goto restart;
@@ -428,7 +435,7 @@ cache_purge(
 }
 
 /*
- * Flush all nodes in the cache to disk. 
+ * Flush all nodes in the cache to disk.
  */
 void
 cache_flush(
@@ -439,13 +446,13 @@ cache_flush(
 	struct list_head *	pos;
 	struct cache_node *	node;
 	int			i;
-	
+
 	if (!cache->flush)
 		return;
-	
+
 	for (i = 0; i < cache->c_hashsize; i++) {
 		hash = &cache->c_hash[i];
-		
+
 		pthread_mutex_lock(&hash->ch_mutex);
 		head = &hash->ch_list;
 		for (pos = head->next; pos != head; pos = pos->next) {
@@ -505,10 +512,10 @@ cache_report(FILE *fp, const char *name, struct cache * cache)
 		total += i*hash_bucket_lengths[i];
 		if (hash_bucket_lengths[i] == 0)
 			continue;
-		fprintf(fp, "Hash buckets with  %2d entries %5ld (%3ld%%)\n", 
+		fprintf(fp, "Hash buckets with  %2d entries %5ld (%3ld%%)\n",
 			i, hash_bucket_lengths[i], (i*hash_bucket_lengths[i]*100)/cache->c_count);
 	}
 	if (hash_bucket_lengths[i])	/* last report bucket is the overflow bucket */
-		fprintf(fp, "Hash buckets with >%2d entries %5ld (%3ld%%)\n", 
+		fprintf(fp, "Hash buckets with >%2d entries %5ld (%3ld%%)\n",
 			i-1, hash_bucket_lengths[i], ((cache->c_count-total)*100)/cache->c_count);
 }

johnpgarry/xfsprogs-dev

commit f9e2fdcf641e1815fe7b461a748d8a913c55c47c

xfs_repair: coordinate parallel updates to the rt bitmap
Actually take the rt lock before updating the bitmap from multiple
threads.  This fixes an infrequent corruption problem when running
generic/013 and rtinherit=1 is set on the root dir.

Fixes: 2556c98bd9e6 ("Perform true sequential bulk read prefetching in xfs_repair Merge of master-melb:xfs-cmds:29147a by kenmcd.")
Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

repair/dinode.c
@@ -323,6 +323,7 @@ process_bmbt_reclist_int(
 	xfs_extlen_t		blen;
 	xfs_agnumber_t		locked_agno = -1;
 	int			error = 1;
+	int			error2;
 
 	if (type == XR_INO_RTDATA)
 		ftype = ftype_real_time;
@@ -383,14 +384,14 @@ _("zero length extent (off = %" PRIu64 ", fsbno = %" PRIu64 ") in ino %" PRIu64 
 		}
 
 		if (type == XR_INO_RTDATA && whichfork == XFS_DATA_FORK) {
+			pthread_mutex_lock(&rt_lock.lock);
+			error2 = process_rt_rec(mp, &irec, ino, tot, check_dups);
+			pthread_mutex_unlock(&rt_lock.lock);
+			if (error2)
+				return error2;
+
 			/*
-			 * realtime bitmaps don't use AG locks, so returning
-			 * immediately is fine for this code path.
-			 */
-			if (process_rt_rec(mp, &irec, ino, tot, check_dups))
-				return 1;
-			/*
-			 * skip rest of loop processing since that'irec.br_startblock
+			 * skip rest of loop processing since the rest is
 			 * all for regular file forks and attr forks
 			 */
 			continue;
@@ -442,7 +443,6 @@ _("inode %" PRIu64 " - extent exceeds max offset - start %" PRIu64 ", "
 		}
 
 		if (blkmapp && *blkmapp) {
-			int	error2;
 			error2 = blkmap_set_ext(blkmapp, irec.br_startoff,
 					irec.br_startblock, irec.br_blockcount);
 			if (error2) {

repair/globals.c
@@ -110,6 +110,7 @@ uint32_t	sb_unit;
 uint32_t	sb_width;
 
 struct aglock	*ag_locks;
+struct aglock	rt_lock;
 
 int		report_interval;
 uint64_t	*prog_rpt_done;

repair/globals.h
@@ -154,6 +154,7 @@ struct aglock {
 	pthread_mutex_t	lock __attribute__((__aligned__(64)));
 };
 extern struct aglock	*ag_locks;
+extern struct aglock	rt_lock;
 
 extern int		report_interval;
 extern uint64_t		*prog_rpt_done;

repair/incore.c
@@ -290,6 +290,7 @@ init_bmaps(xfs_mount_t *mp)
 		btree_init(&ag_bmap[i]);
 		pthread_mutex_init(&ag_locks[i].lock, NULL);
 	}
+	pthread_mutex_init(&rt_lock.lock, NULL);
 
 	init_rt_bmap(mp);
 	reset_bmaps(mp);