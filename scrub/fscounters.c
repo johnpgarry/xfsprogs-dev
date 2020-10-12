2b9857cdb566359cf48d277c5b841cb102c7cbd2
Darrick J. Wong <darrick.wong@oracle.com>
2020-10-12 15:39:52 -0400

    xfs_scrub: don't use statvfs to collect filesystem summary counts
    The function scrub_scan_estimate_blocks naïvely uses the statvfs counts
    to estimate the size and free blocks on the data volume.  Unfortunately,
    it fails to account for the fact that statvfs can return the size and
    free counts for the realtime volume if the root directory has the
    rtinherit flag set, which leads to phase 7 reporting totally absurd
    quantities.
    
    Eric pointed out a further problem with statvfs, which is that the file
    counts are clamped to the current user's project quota inode limits.
    Therefore, we must not use statvfs for querying the filesystem summary
    counts.
    
    The XFS_IOC_FSCOUNTS ioctl returns all the data we need, so use that
    instead.
    
    Fixes: 604dd3345f35 ("xfs_scrub: filesystem counter collection functions")
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>
    Reviewed-by: Eric Sandeen <sandeen@redhat.com>
    Signed-off-by: Eric Sandeen <sandeen@sandeen.net>

--- scrub/fscounters.c
+++ scrub/fscounters.c
@@ -130,38 +130,19 @@ scrub_scan_estimate_blocks(
 	unsigned long long		*f_free)
 {
 	struct xfs_fsop_counts		fc;
-	struct xfs_fsop_resblks		rb;
-	struct statvfs			sfs;
 	int				error;
-
-	/* Grab the fstatvfs counters, since it has to report accurately. */
-	error = fstatvfs(ctx->mnt.fd, &sfs);
-	if (error)
-		return errno;
 
 	/* Fetch the filesystem counters. */
 	error = ioctl(ctx->mnt.fd, XFS_IOC_FSCOUNTS, &fc);
 	if (error)
 		return errno;
 
-	/*
-	 * XFS reserves some blocks to prevent hard ENOSPC, so add those
-	 * blocks back to the free data counts.
-	 */
-	error = ioctl(ctx->mnt.fd, XFS_IOC_GET_RESBLKS, &rb);
-	if (error)
-		return errno;
-
-	sfs.f_bfree += rb.resblks_avail;
-
-	*d_blocks = sfs.f_blocks;
-	if (ctx->mnt.fsgeom.logstart > 0)
-		*d_blocks += ctx->mnt.fsgeom.logblocks;
-	*d_bfree = sfs.f_bfree;
+	*d_blocks = ctx->mnt.fsgeom.datablocks;
+	*d_bfree = fc.freedata;
 	*r_blocks = ctx->mnt.fsgeom.rtblocks;
 	*r_bfree = fc.freertx;
-	*f_files = sfs.f_files;
-	*f_free = sfs.f_ffree;
+	*f_files = fc.allocino;
+	*f_free = fc.freeino;
 
 	return 0;
 }
