06963ef0d4ff9a6736f699d1ca8ef5a0c194799b
Darrick J. Wong <darrick.wong@oracle.com>
2020-11-12 16:49:42 -0500

    xfs: widen ondisk quota expiration timestamps to handle y2038+
    Source kernel commit: 4ea1ff3b49681af45a4a8c14baf7f0b3d11aa74a
    
    Enable the bigtime feature for quota timers.  We decrease the accuracy
    of the timers to ~4s in exchange for being able to set timers up to the
    bigtime maximum.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Allison Collins <allison.henderson@oracle.com>
    Reviewed-by: Dave Chinner <dchinner@redhat.com>
    Signed-off-by: Eric Sandeen <sandeen@sandeen.net>

--- include/xfs_mount.h
+++ include/xfs_mount.h
@@ -184,4 +184,8 @@ extern xfs_mount_t	*libxfs_mount (xfs_mount_t *, xfs_sb_t *,
 int		libxfs_umount(struct xfs_mount *mp);
 extern void	libxfs_rtmount_destroy (xfs_mount_t *);
 
+/* Dummy xfs_dquot so that libxfs compiles. */
+struct xfs_dquot {
+	int		q_type;
+};
 #endif	/* __XFS_MOUNT_H__ */
