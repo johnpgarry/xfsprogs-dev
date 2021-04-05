3006cea4c9418998b2cdea3ac65cd78809aa81ee
Chandan Babu R <chandanrlinux@gmail.com>
2021-04-05 18:36:56 -0400

    xfs: Introduce error injection to allocate only minlen size extents for files
    Source kernel commit: 301519674699aa9b80a15b2b2165e08532b176e6
    
    This commit adds XFS_ERRTAG_BMAP_ALLOC_MINLEN_EXTENT error tag which
    helps userspace test programs to get xfs_bmap_btalloc() to always
    allocate minlen sized extents.
    
    This is required for test programs which need a guarantee that minlen
    extents allocated for a file do not get merged with their existing
    neighbours in the inode's BMBT. "Inode fork extent overflow check" for
    Directories, Xattrs and extension of realtime inodes need this since the
    file offset at which the extents are being allocated cannot be
    explicitly controlled from userspace.
    
    One way to use this error tag is to,
    1. Consume all of the free space by sequentially writing to a file.
    2. Punch alternate blocks of the file. This causes CNTBT to contain
    sufficient number of one block sized extent records.
    3. Inject XFS_ERRTAG_BMAP_ALLOC_MINLEN_EXTENT error tag.
    After step 3, xfs_bmap_btalloc() will issue space allocation
    requests for minlen sized extents only.
    
    ENOSPC error code is returned to userspace when there aren't any "one
    block sized" extents left in any of the AGs.
    
    Reviewed-by: Darrick J. Wong <darrick.wong@oracle.com>
    Signed-off-by: Chandan Babu R <chandanrlinux@gmail.com>
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>
    Signed-off-by: Eric Sandeen <sandeen@sandeen.net>

--- io/inject.c
+++ io/inject.c
@@ -56,6 +56,7 @@ error_tag(char *name)
 		{ XFS_ERRTAG_IUNLINK_FALLBACK,		"iunlink_fallback" },
 		{ XFS_ERRTAG_BUF_IOERROR,		"buf_ioerror" },
 		{ XFS_ERRTAG_REDUCE_MAX_IEXTENTS,	"reduce_max_iextents" },
+		{ XFS_ERRTAG_BMAP_ALLOC_MINLEN_EXTENT,	"bmap_alloc_minlen_extent" },
 		{ XFS_ERRTAG_MAX,			NULL }
 	};
 	int	count;
