7762bf809c0f7162364373cabef869f51e633a17
Christoph Hellwig <hch@lst.de>
2021-05-05 09:11:22 -0700

    xfs: move the di_crtime field to struct xfs_inode
    Source kernel commit: e98d5e882b3ccb0f7f38d4e893fe60c1dd7934db
    
    Move the crtime field from struct xfs_icdinode into stuct xfs_inode and
    remove the now entirely unused struct xfs_icdinode.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Darrick J. Wong <djwong@kernel.org>
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- include/xfs_inode.h
+++ include/xfs_inode.h
@@ -90,7 +90,7 @@ typedef struct xfs_inode {
 	uint8_t			i_forkoff;	/* attr fork offset >> 3 */
 	uint16_t		i_diflags;	/* XFS_DIFLAG_... */
 	uint64_t		i_diflags2;	/* XFS_DIFLAG2_... */
-	struct xfs_icdinode	i_d;		/* most of ondisk inode */
+	struct timespec64	i_crtime;	/* time created */
 
 	xfs_extnum_t		i_cnextents;	/* # of extents in cow fork */
 	unsigned int		i_cformat;	/* format of cow fork */
