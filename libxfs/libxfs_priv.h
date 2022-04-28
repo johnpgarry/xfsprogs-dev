7d6b86acc14d44cd08d5430c422ba420711a705b
Darrick J. Wong <djwong@kernel.org>
2022-04-28 15:39:02 -0400

    xfs: port the defer ops capture and continue to resource capture
    Source kernel commit: 512edfac85d243ed6a5a5f42f513ebb7c2d32863
    
    When log recovery tries to recover a transaction that had log intent
    items attached to it, it has to save certain parts of the transaction
    state (reservation, dfops chain, inodes with no automatic unlock) so
    that it can finish single-stepping the recovered transactions before
    finishing the chains.
    
    This is done with the xfs_defer_ops_capture and xfs_defer_ops_continue
    functions.  Right now they open-code this functionality, so let's port
    this to the formalized resource capture structure that we introduced in
    the previous patch.  This enables us to hold up to two inodes and two
    buffers during log recovery, the same way we do for regular runtime.
    
    With this patch applied, we'll be ready to support atomic extent swap
    which holds two inodes; and logged xattrs which holds one inode and one
    xattr leaf buffer.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>
    Reviewed-by: Allison Henderson <allison.henderson@oracle.com>
    Signed-off-by: Eric Sandeen <sandeen@sandeen.net>

--- libxfs/libxfs_priv.h
+++ libxfs/libxfs_priv.h
@@ -467,6 +467,7 @@ void __xfs_buf_mark_corrupt(struct xfs_buf *bp, xfs_failaddr_t fa);
 	typeof(mode) __mode = mode;				\
 	__mode = __mode; /* no set-but-unused warning */	\
 })
+#define xfs_lock_two_inodes(ip0,mode0,ip1,mode1)	((void) 0)
 
 /* space allocation */
 #define XFS_EXTENT_BUSY_DISCARDED	0x01	/* undergoing a discard op. */
