22f64346ed74e6b9687f3a7bc14f90f97ab2cfef
Darrick J. Wong <djwong@kernel.org>
2022-04-28 15:39:03 -0400

    xfs: dynamically allocate cursors based on maxlevels
    Source kernel commit: c940a0c54a2e9333478f1d87ed40006a04fcec7e
    
    To support future btree code, we need to be able to size btree cursors
    dynamically for very large btrees.  Switch the maxlevels computation to
    use the precomputed values in the superblock, and create cursors that
    can handle a certain height.  For now, we retain the btree cursor cache
    that can handle up to 9-level btrees, though a subsequent patch
    introduces separate caches for each btree type, where each cache's
    objects will be exactly tall enough to handle the specific btree type.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>
    Reviewed-by: Dave Chinner <dchinner@redhat.com>
    Signed-off-by: Eric Sandeen <sandeen@sandeen.net>

--- libxfs/init.c
+++ libxfs/init.c
@@ -242,7 +242,7 @@ init_zones(void)
 	xfs_da_state_zone = kmem_zone_init(
 			sizeof(struct xfs_da_state), "xfs_da_state");
 	xfs_btree_cur_zone = kmem_zone_init(
-			xfs_btree_cur_sizeof(XFS_BTREE_MAXLEVELS),
+			xfs_btree_cur_sizeof(XFS_BTREE_CUR_CACHE_MAXLEVELS),
 			"xfs_btree_cur");
 	xfs_bmap_free_item_zone = kmem_zone_init(
 			sizeof(struct xfs_extent_free_item),
