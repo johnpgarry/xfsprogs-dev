e501cb4490e3e3523a28aca09e49eea3058f99a4
Darrick J. Wong <djwong@kernel.org>
2022-05-06 15:22:35 -0400

    xfs: remove the XFS_IOC_FSSETDM definitions
    Source kernel commit: 9dec0368b9640c09ef5af48214e097245e57a204
    
    Remove the definitions for these ioctls, since the functionality (and,
    weirdly, the 32-bit compat ioctl definitions) were removed from the
    kernel in November 2019.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>
    Reviewed-by: Dave Chinner <dchinner@redhat.com>
    Signed-off-by: Eric Sandeen <sandeen@sandeen.net>

--- libhandle/handle.c
+++ libhandle/handle.c
@@ -436,30 +436,15 @@ parentpaths_by_handle(
 	return -1;
 }
 
+/* Deprecated in kernel */
 int
 fssetdm_by_handle(
 	void		*hanp,
 	size_t		hlen,
 	struct fsdmidata *fsdmidata)
 {
-	int		fd;
-	char		*path;
-	xfs_fsop_setdm_handlereq_t dmhreq;
-
-	if ((fd = handle_to_fsfd(hanp, &path)) < 0)
-		return -1;
-
-	dmhreq.hreq.fd       = 0;
-	dmhreq.hreq.path     = NULL;
-	dmhreq.hreq.oflags   = O_LARGEFILE;
-	dmhreq.hreq.ihandle  = hanp;
-	dmhreq.hreq.ihandlen = hlen;
-	dmhreq.hreq.ohandle  = NULL;
-	dmhreq.hreq.ohandlen = NULL;
-
-	dmhreq.data = fsdmidata;
-
-	return xfsctl(path, fd, XFS_IOC_FSSETDM_BY_HANDLE, &dmhreq);
+	errno = EOPNOTSUPP;
+	return -1;
 }
 
 /*ARGSUSED1*/
