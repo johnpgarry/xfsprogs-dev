bc522905c5d712405ceec1c05b1bcc85cdbe65e3
Allison Henderson <allison.henderson@oracle.com>
2022-06-22 14:28:52 -0500

    xfs: Add helper function xfs_attr_leaf_addname
    Source kernel commit: cd1549d6df22e4f72903dbb169202203d429bcff
    
    This patch adds a helper function xfs_attr_leaf_addname.  While this
    does help to break down xfs_attr_set_iter, it does also hoist out some
    of the state management.  This patch has been moved to the end of the
    clean up series for further discussion.
    
    Suggested-by: Darrick J. Wong <djwong@kernel.org>
    Signed-off-by: Allison Henderson <allison.henderson@oracle.com>
    Reviewed-by: Darrick J. Wong <djwong@kernel.org>
    Reviewed-by: Chandan Babu R <chandanrlinux@gmail.com>
    Signed-off-by: Dave Chinner <david@fromorbit.com>
    Signed-off-by: Eric Sandeen <sandeen@sandeen.net>

--- include/xfs_trace.h
+++ include/xfs_trace.h
@@ -41,6 +41,7 @@
 
 #define trace_xfs_attr_sf_addname_return(...)	((void) 0)
 #define trace_xfs_attr_set_iter_return(...)	((void) 0)
+#define trace_xfs_attr_leaf_addname_return(...)	((void) 0)
 #define trace_xfs_attr_node_addname_return(...)	((void) 0)
 #define trace_xfs_attr_remove_iter_return(...)	((void) 0)
 #define trace_xfs_attr_rmtval_remove_return(...) ((void) 0)
