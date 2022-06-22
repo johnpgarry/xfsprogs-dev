fc32183a1e6d9817ef7a4c0fcd11883a0e17c967
Dave Chinner <dchinner@redhat.com>
2022-06-22 14:28:52 -0500

    xfs: switch attr remove to xfs_attri_set_iter
    Source kernel commit: 4b9879b19cafa63ae02fef30f678b2179a648d45
    
    Now that xfs_attri_set_iter() has initial states for removing
    attributes, switch the pure attribute removal code over to using it.
    This requires attrs being removed to always be marked as INCOMPLETE
    before we start the removal due to the fact we look up the attr to
    remove again in xfs_attr_node_remove_attr().
    
    Note: this drops the fillstate/refillstate optimisations from
    the remove path that avoid having to look up the path again after
    setting the incomplete flag and removing remote attrs. Restoring
    that optimisation to this path is future Dave's problem.
    
    Signed-off-by: Dave Chinner <dchinner@redhat.com>
    Reviewed-by: Allison Henderson <allison.henderson@oracle.com>
    Reviewed-by: Darrick J. Wong <djwong@kernel.org>
    Signed-off-by: Dave Chinner <david@fromorbit.com>
    Signed-off-by: Eric Sandeen <sandeen@sandeen.net>

--- libxfs/defer_item.c
+++ libxfs/defer_item.c
@@ -496,11 +496,9 @@ xfs_attr_finish_item(
 	struct xfs_attr_item	*attr;
 	int			error;
 	struct xfs_da_args	*args;
-	unsigned int		op;
 
 	attr = container_of(item, struct xfs_attr_item, xattri_list);
 	args = attr->xattri_da_args;
-	op = attr->xattri_op_flags & XFS_ATTR_OP_FLAGS_TYPE_MASK;
 
 	/*
 	 * Always reset trans after EAGAIN cycle
@@ -513,18 +511,9 @@ xfs_attr_finish_item(
 		goto out;
 	}
 
-	switch (op) {
-	case XFS_ATTR_OP_FLAGS_SET:
-		error = xfs_attr_set_iter(attr);
-		break;
-	case XFS_ATTR_OP_FLAGS_REMOVE:
-		ASSERT(XFS_IFORK_Q(args->dp));
-		error = xfs_attr_remove_iter(attr);
-		break;
-	default:
-		error = -EFSCORRUPTED;
-		break;
-	}
+	error = xfs_attr_set_iter(attr);
+	if (!error && attr->xattri_dela_state != XFS_DAS_DONE)
+		error = -EAGAIN;
 out:
 	if (error != -EAGAIN)
 		kmem_free(attr);
