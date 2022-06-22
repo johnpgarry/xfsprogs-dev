54a1aecb972e3a75fc213a1ac666c4769109436a
Chandan Babu R <chandan.babu@oracle.com>
2022-06-22 14:28:52 -0500

    xfs: Introduce XFS_FSOP_GEOM_FLAGS_NREXT64
    Source kernel commit: 7c05aa9d9d2014937c8dacbd514bca2592b11f48
    
    XFS_FSOP_GEOM_FLAGS_NREXT64 indicates that the current filesystem instance
    supports 64-bit per-inode extent counters.
    
    Reviewed-by: Darrick J. Wong <djwong@kernel.org>
    Reviewed-by: Dave Chinner <dchinner@redhat.com>
    Signed-off-by: Chandan Babu R <chandan.babu@oracle.com>
    Signed-off-by: Eric Sandeen <sandeen@sandeen.net>

--- libxfs/xfs_sb.c
+++ libxfs/xfs_sb.c
@@ -1136,6 +1136,8 @@ xfs_fs_geometry(
 	} else {
 		geo->logsectsize = BBSIZE;
 	}
+	if (xfs_has_large_extent_counts(mp))
+		geo->flags |= XFS_FSOP_GEOM_FLAGS_NREXT64;
 	geo->rtsectsize = sbp->sb_blocksize;
 	geo->dirblocksize = xfs_dir2_dirblock_bytes(sbp);
 
