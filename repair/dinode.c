528613d30a1b3ed6668a4776a21e607ca01d6654
Darrick J. Wong <djwong@kernel.org>
2022-07-15 14:22:04 -0700

    xfs_repair: check metadata inode flag
    Check whether or not the metadata inode flag is set appropriately.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- repair/dinode.c
+++ repair/dinode.c
@@ -2669,6 +2669,20 @@ _("bad (negative) size %" PRId64 " on inode %" PRIu64 "\n"),
 			}
 		}
 
+		if (flags2 & XFS_DIFLAG2_METADIR) {
+			xfs_failaddr_t	fa;
+
+			fa = libxfs_dinode_verify_metadir(mp, dino, di_mode,
+					be16_to_cpu(dino->di_flags), flags2);
+			if (fa) {
+				if (!uncertain)
+					do_warn(
+	_("inode %" PRIu64 " is incorrectly marked as metadata\n"),
+						lino);
+				goto clear_bad_out;
+			}
+		}
+
 		if ((flags2 & XFS_DIFLAG2_REFLINK) &&
 		    !xfs_has_reflink(mp)) {
 			if (!uncertain) {
