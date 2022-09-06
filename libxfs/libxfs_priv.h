f7f3ea41a0613154733f39729545a2a9507ba805
Darrick J. Wong <djwong@kernel.org>
2022-09-06 09:40:40 -0700

    libxfs: consume the xfs_warn mountpoint argument
    Fix these warnings because xfs_warn doesn't do anything in userspace:
    
    xfs_alloc.c: In function ‘xfs_alloc_get_rec’:
    xfs_alloc.c:246:34: warning: unused variable ‘mp’ [-Wunused-variable]
      246 |         struct xfs_mount        *mp = cur->bc_mp;
          |                                  ^~
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/libxfs_priv.h
+++ libxfs/libxfs_priv.h
@@ -125,7 +125,7 @@ enum ce { CE_DEBUG, CE_CONT, CE_NOTE, CE_WARN, CE_ALERT, CE_PANIC };
 
 #define xfs_info(mp,fmt,args...)	cmn_err(CE_CONT, _(fmt), ## args)
 #define xfs_notice(mp,fmt,args...)	cmn_err(CE_NOTE, _(fmt), ## args)
-#define xfs_warn(mp,fmt,args...)	cmn_err(CE_WARN, _(fmt), ## args)
+#define xfs_warn(mp,fmt,args...)	cmn_err((mp) ? CE_WARN : CE_WARN, _(fmt), ## args)
 #define xfs_err(mp,fmt,args...)		cmn_err(CE_ALERT, _(fmt), ## args)
 #define xfs_alert(mp,fmt,args...)	cmn_err(CE_ALERT, _(fmt), ## args)
 
