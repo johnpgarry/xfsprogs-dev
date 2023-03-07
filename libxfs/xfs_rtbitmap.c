513be319589c34d298696873abcb2686abfb6738
Darrick J. Wong <djwong@kernel.org>
2023-03-06 19:55:42 -0800

    xfs: support error injection when freeing rt extents
    A handful of fstests expect to be able to test what happens when extent
    free intents fail to actually free the extent.  Now that we're
    supporting EFIs for realtime extents, add to xfs_rtfree_extent the same
    injection point that exists in the regular extent freeing code.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_rtbitmap.c
+++ libxfs/xfs_rtbitmap.c
@@ -16,6 +16,7 @@
 #include "xfs_trans.h"
 #include "xfs_rtbitmap.h"
 #include "xfs_health.h"
+#include "xfs_errortag.h"
 
 /*
  * Realtime allocator bitmap functions shared with userspace.
@@ -1035,6 +1036,9 @@ xfs_rtfree_extent(
 
 	ASSERT(mp->m_rbmip->i_itemp != NULL);
 	ASSERT(xfs_isilocked(mp->m_rbmip, XFS_ILOCK_EXCL));
+
+	if (XFS_TEST_ERROR(false, mp, XFS_ERRTAG_FREE_EXTENT))
+		return -EIO;
 
 	error = xfs_rtcheck_alloc_range(&args, start, len);
 	if (error)
