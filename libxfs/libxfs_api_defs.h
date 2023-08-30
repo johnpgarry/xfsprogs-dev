1f6330bc6fdc3a2aef9fcfde44bc381eb805a556
Darrick J. Wong <djwong@kernel.org>
2023-08-30 11:56:03 -0700

    xfs_db: port the iunlink command to use the libxfs iunlink function
    Now that we've ported the kernel's iunlink code to userspace, adapt the
    debugger command to use it instead of duplicating the logic.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/libxfs_api_defs.h
+++ libxfs/libxfs_api_defs.h
@@ -185,6 +185,7 @@
 
 #define xfs_iread_extents		libxfs_iread_extents
 #define xfs_irele			libxfs_irele
+#define xfs_iunlink			libxfs_iunlink
 #define xfs_link_space_res		libxfs_link_space_res
 #define xfs_log_calc_minimum_size	libxfs_log_calc_minimum_size
 #define xfs_log_get_max_trans_res	libxfs_log_get_max_trans_res
