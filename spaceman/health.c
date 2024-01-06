kernelorg-mirror/linux_kernel_git_djwong_xfsprogs-dev

xfs_spaceman: report health of the realtime refcount btree
Report the health of the realtime reference count btree.

Signed-off-by: Darrick J. Wong <djwong@kernel.org>

spaceman/health.c
@@ -44,6 +44,11 @@ static bool has_rtrmapbt(const struct xfs_fsop_geom *g)
 	return g->rtblocks > 0 && (g->flags & XFS_FSOP_GEOM_FLAGS_RMAPBT);
 }
 
+static bool has_rtreflink(const struct xfs_fsop_geom *g)
+{
+	return g->rtblocks > 0 && (g->flags & XFS_FSOP_GEOM_FLAGS_REFLINK);
+}
+
 struct flag_map {
 	unsigned int		mask;
 	bool			(*has_fn)(const struct xfs_fsop_geom *g);
@@ -89,6 +94,11 @@ static const struct flag_map fs_flags[] = {
 		.mask = XFS_FSOP_GEOM_SICK_RT_RMAPBT,
 		.descr = "realtime reverse mappings btree",
 		.has_fn = has_rtrmapbt,
+	},
+	{
+		.mask = XFS_FSOP_GEOM_SICK_RT_REFCNTBT,
+		.descr = "realtime reference count btree",
+		.has_fn = has_rtreflink,
 	},
 	{0},
 };

johnpgarry/xfsprogs-dev

spaceman/health.c
@@ -46,6 +46,11 @@ static bool has_rtrmapbt(const struct xfs_fsop_geom *g)
 	return g->rtblocks > 0 && (g->flags & XFS_FSOP_GEOM_FLAGS_RMAPBT);
 }
 
+static bool has_rtreflink(const struct xfs_fsop_geom *g)
+{
+	return g->rtblocks > 0 && (g->flags & XFS_FSOP_GEOM_FLAGS_REFLINK);
+}
+
 struct flag_map {
 	unsigned int		mask;
 	bool			(*has_fn)(const struct xfs_fsop_geom *g);
@@ -154,6 +159,11 @@ static const struct flag_map rtgroup_flags[] = {
 		.mask = XFS_RTGROUP_GEOM_SICK_RMAPBT,
 		.descr = "realtime reverse mappings btree",
 		.has_fn = has_rtrmapbt,
+	},
+	{
+		.mask = XFS_RTGROUP_GEOM_SICK_REFCNTBT,
+		.descr = "realtime reference count btree",
+		.has_fn = has_rtreflink,
 	},
 	{0},
 };