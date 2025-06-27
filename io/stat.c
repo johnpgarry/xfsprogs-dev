a4b1950b6be075dc5d85a9081efe334e49296d14
John Garry <john.g.garry@oracle.com>
2025-06-27 12:05:05 +0100

    bodge atomic write unit max opt support

--- io/stat.c
+++ io/stat.c
@@ -410,6 +410,7 @@ dump_raw_statx(struct statx *stx)
 	printf("stat.subvol = 0x%llu\n", (unsigned long long)stx->stx_subvol);
 	printf("stat.atomic_write_unit_min = %u\n", stx->stx_atomic_write_unit_min);
 	printf("stat.atomic_write_unit_max = %u\n", stx->stx_atomic_write_unit_max);
+	printf("stat.stx_atomic_write_unit_max_opt = %u\n", stx->stx_atomic_write_unit_max_opt);
 	printf("stat.atomic_write_segments_max = %u\n", stx->stx_atomic_write_segments_max);
 	printf("stat.dio_read_offset_align = %u\n", stx->stx_dio_read_offset_align);
 	return 0;
